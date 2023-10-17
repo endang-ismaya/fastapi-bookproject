9d0ed3737d4325151cf0fe8239b98eb775436466
Endang Ismaya <endang.ismaya@gmail.com>
2023-10-17 18:45:45 +0700

    add auth user from form

--- src/routers/auth_route.py
+++ src/routers/auth_route.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 from src.models.UserRequest import CreateUserRequest
 from src.database.models import Users
@@ -8,6 +10,17 @@ from passlib.context import CryptContext
 router = APIRouter()
 
 bcrypt_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.email == username).first()
+    if not user:
+        return False
+
+    if not bcrypt_ctx.verify(password, user.hashed_password):
+        return False
+
+    return True
 
 
 @router.post("/auth/create", status_code=status.HTTP_201_CREATED)
@@ -23,3 +36,15 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 
     db.add(cr_user_model)
     db.commit()
+
+
+@router.post("/auth/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        return "Failed Authentication"
+
+    return "Success Auth"
